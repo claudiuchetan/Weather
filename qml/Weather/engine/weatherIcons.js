649375aaa39c951be65cfb49d444cf5c7b1e6e8d
clchetan <claudiu.chetan@nokia.com>
2011-03-04 11:29:16 +0200

    icons mapping

--- qml/Weather/engine/weatherIcons.js
+++ qml/Weather/engine/weatherIcons.js
@@ -1,44 +1,97 @@
-var weatherIcon={
-    "clear": {
-            width:161,
-            height:161,
-            src:"clear.png",
-            night:false
+var weatherIcon=[
+    {
+        name: "clear",
+        width:161,
+        height:161,
+        src:"clear.png",
+        night:false,
+        codes: [113]},
+    {
+        name: "cloudy",
+        width:317,
+        height:235,
+        src:"cloudy.png",
+        night:false,
+        codes: [116]
         },
-    "cloudy": {
-            width:317,
-            height:235,
-            src:"cloudy.png",
-            night:false
+    {
+        name: "clouds",
+        width:312,
+        height:216,
+        src:"clouds.png",
+        night:false,
+        codes: [122, 119]
         },
-    "clouds": {
-            width:312,
-            height:216,
-            src:"clouds.png",
-            night:false
+    {
+        name: "rain",
+        width:312,
+        height:265,
+        src:"rain.png",
+        night:false,
+        codes: [386, 353, 311, 302, 299, 296, 293, 284, 281, 266, 263, 185, 182, 179]
         },
-    "rain": {
-            width:312,
-            height:265,
-            src:"rain.png",
-            night:false
+    {
+        name: "storm",
+        width:312,
+        height:265,
+        src:"storm.png",
+        night:false,
+        codes: [389, 359, 356, 314, 308, 305]
         },
-    "clear_night": {
-            width:170,
-            height:170,
-            src:"moon.png",
-            night:true
+    {
+        name: "snow",
+        width:312,
+        height:265,
+        src:"snow.png",
+        night:false,
+        codes: [395, 392, 377, 374, 371, 368, 365, 362, 350, 338, 335, 332, 329, 326, 323, 320, 317, 230, 227]
         },
-    "cloudy_night": {
-            width:325,
-            height:241,
-            src:"cloudy_night.png",
-            night:true
+    {
+        name: "clear_night",
+        width:170,
+        height:170,
+        src:"moon.png",
+        night:true,
+        codes: [113]
         },
-    "clouds_night": {
-            width:312,
-            height:216,
-            src:"clouds_night.png",
-            night:true
+    {
+        name: "cloudy_night",
+        width:325,
+        height:241,
+        src:"cloudy_night.png",
+        night:true,
+        codes: [116]
+        },
+    {
+        name: "clouds_night",
+        width:312,
+        height:216,
+        src:"clouds_night.png",
+        night:true,
+        codes: [122, 119]
+        },
+    {
+        name: "rain_night",
+        width:312,
+        height:265,
+        src:"rain.png",
+        night:false,
+        codes: [386, 353, 311, 302, 299, 296, 293, 284, 281, 266, 263, 185, 182, 179]
+        },
+    {
+        name: "storm_night",
+        width:312,
+        height:265,
+        src:"storm.png",
+        night:false,
+        codes: [389, 359, 356, 314, 308, 305]
+        },
+    {
+        name: "snow_night",
+        width:312,
+        height:265,
+        src:"snow.png",
+        night:false,
+        codes: [395, 392, 377, 374, 371, 368, 365, 362, 350, 338, 335, 332, 329, 326, 323, 320, 317, 230, 227]
         }
-    }
+    ]
